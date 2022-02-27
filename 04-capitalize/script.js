013526112c457a5e4ad57ec9f3c2a02c74a9d427
RomanMitaki <r.mitaki@yandex.ru>
2022-02-27 18:38:01 +0300

    3-5 tasks are done

--- 04-capitalize/script.js
+++ 04-capitalize/script.js
@@ -4,12 +4,29 @@
  * Напишите функцию capitalize(str). Функция должна возвращать новую
  * строку каждое слово в которой начинается с прописной буквы.
  *
-*/
+ */
 
 function capitalize(str) {
-    // Напишите код здесь
+  if (str !== String(str)) {
+    return str;
+  } else if (str.length === 0) {
+    return str;
+  } else if (str.trim() === "") {
+    return str;
+  } else {
+    const trimStr = str.trim();
+    const strArr = trimStr.split(" ");
+    const filterArr = strArr.filter((item) => {
+      return item !== "";
+    });
+    const modifiedStrArr = filterArr.map((item) => {
+      return item[0].toUpperCase() + item.slice(1);
+    });
+
+    return modifiedStrArr.join(" ");
+  }
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(capitalize('молодость всё простит')); // "Молодость Всё Простит"
+console.log(capitalize("ddddddd  ffffdf")); // "Молодость Всё Простит"
